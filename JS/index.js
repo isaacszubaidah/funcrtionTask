76f3da69f6cd04f6c8d12f1a36d714a3df883387
isaacszubaidah <isaacszubaidah17@gmail.com>
2023-11-20 10:11:08 +0200

    add

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+function greet(name){
+    console.log(`Hello ${name}`)};
+    greet('Zubaidah');
+function multiply(input1,input2){
+   answer = input1 * input2;
+   console.log(answer);
+}
+multiply(10,5)
+
+function tableOfFive(number){
+   let result = number * 5
+    console.log(result)
+    return result
+}
+tableOfFive(7)
+
+let multiplyValue = tableOfFive(7);
+console.log(multiplyValue);
